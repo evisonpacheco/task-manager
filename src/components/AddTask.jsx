964b18821e9bb024a5e85098baf04668a3a77369
Evison <evison.pacheco21@gmail.com>
2025-01-07 16:10:47 -0300

    update: adicionando componente AddTasks

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,5 +1,11 @@
 function AddTask() {
-
+  return (
+  <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+    <input type="text" placeholder="Adicionar Tarefa" className="border border-slate-300 outline-slate-400 px-4 py-2 rouded-md"/>
+    <input type="text" placeholder="Adicionar Descrição" className="border border-slate-300 outline-slate-400 px-4 py-2 rouded-md"/>
+    <button className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium">Adicionar</button>
+  </div>
+  );
 }
 
-export default AddTask
+export default AddTask;
